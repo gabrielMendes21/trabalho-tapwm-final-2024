eda04aae0e7549d390533ca8acebdc1809d6a1a8
Gabriel Mendes <gabriel.26056@gmail.com>
2024-11-29 19:11:54 -0300

    add mobile project

--- src/lib/admin.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-require("dotenv").config();
-const serviceAccount = require("../../firebase-secret-key.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
